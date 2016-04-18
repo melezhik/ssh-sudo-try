cd38e2e1bf2ab08474951585c037149d1682a906
Alexey Melezhik <melezhik@gmail.com>
2016-04-18 15:33:09 +0300

    refactoring

--- modules/ssh-and-sudo/story.pm
+++ modules/ssh-and-sudo/story.pm
@@ -1,6 +1,16 @@
 my $ssh_command = config()->{'ssh-sudo-try'}->{ssh_command};
 my $sudo_command = config()->{'ssh-sudo-try'}->{sudo_command};
 my $server = story_var('server');
-my $command = "$ssh_command $server $sudo_command >/dev/null; echo -n status: \$?";
-set_stdout("$command :".`$command`);
+
+
+set_stdout('ssh_begin');
+my $command = "$ssh_command $server $sudo_command 2>\&1; echo status: \$?";
+
+set_stdout($command);
+
+for my $l (split /\n/, `$command`){
+  set_stdout($l);
+}
+
+set_stdout('ssh_end');
 
